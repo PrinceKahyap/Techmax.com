0641afd3c40b8f291f4a6d12ea23b8ff6056b1f7
Princekashyap <vasukashyap08@gmail.com>
2023-02-01 19:27:09 +0530

    This project is complete i code all the page and also responsive way

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Footer-Copyright 
 let year = document.querySelector("#current-year")
-year.innerHTML = new Date() .getFullYear();
+year.innerHTML = new Date().getFullYear();
 
 // Slider-owlCarousel 
 $(".slider").owlCarousel({
@@ -9,21 +9,21 @@ $(".slider").owlCarousel({
     autoplay: true,
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
-    responsive:{
-     0:{
-         items: 1,
-         nav: false
-     },
-     600:{
-         items: 2,
-         nav: false
-     },
-     1000:{
-         items: 3,
-         nav: false
-     },
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        },
     }
- });
+});
 
 // Slider-owlCarousel 
 $(".carousel").owlCarousel({
@@ -32,48 +32,49 @@ $(".carousel").owlCarousel({
     autoplay: true,
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
-    responsive:{
-     0:{
-         items: 1,
-         nav: false
-     },
-     600:{
-         items: 4,
-         nav: false
-     },
-     1000:{
-         items: 5,
-         nav: false
-     },
+    responsive: {
+        0: {
+            items: 2,
+            nav: false
+        },
+        600: {
+            items: 4,
+            nav: false
+        },
+        1000: {
+            items: 5,
+            nav: false
+        },
     }
- });
+});
 
 //  Increment-numbers 
- const counters = document.querySelectorAll('.counter')
- counters.forEach(counter => {
-     counter.innerText = '0'
-     const updateCounter = () => {
-         const target = +counter.getAttribute('data-target')
-         const c = +counter.innerText
-         const increment = target / 200
-         if (c < target) {
-             counter.innerText = `${Math.ceil(c + increment)}`
-             setTimeout(updateCounter, 20)
-         } else {
-             counter.innerText = target
-         }
-     }
-     updateCounter()
- });
+const counters = document.querySelectorAll('.counter')
+counters.forEach(counter => {
+    counter.innerText = '0'
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+        const increment = target / 200
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 20)
+        } else {
+            counter.innerText = target
+        }
+    }
+    updateCounter()
+});
 
- let mybutton = document.getElementById("myBtn");
+// Toptoscroll 
+let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
-function scrollFunction(){
-    if(document.body.scrollTop > 500 || document.documentElement.scrollTop >500){
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         mybutton.style.display = "block";
-    }else{
+    } else {
         mybutton.style.display = "none";
     }
 }
@@ -82,17 +83,27 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 };
 
-window.addEventListener("scroll",function(){
+// Sticky Navbar 
+window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
-    header.classList.toggle("sticky",window.scrollY >200);
+    header.classList.toggle("sticky", window.scrollY > 200);
 });
 
+// Hamburger 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click",mobileMenu);
+hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu(){
+function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 };
